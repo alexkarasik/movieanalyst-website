b74f89fde309a7f79fa39a0a430121f9faaabfcf
kinkorea <alexkarasik89@gmail.com>
2017-07-05 14:49:25 -0400

    Server set up complete

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.set('views', _dirname + '/public/views/');
 
 app.user(express. static(_dirname + '/public'));
 
-let NON_INTERACTIVE_CLIENT_ID = 'YOUR-AUTHO-CLIENT-ID';
-let NON_INTERACTIVE_CLIENT_SECRET = 'YOUR-AUTHO-CLIENT-SECRET';
+let NON_INTERACTIVE_CLIENT_ID = 'QxDZw1Jls9Bb7ol0oqj1zxG1IxWFNSlM';
+let NON_INTERACTIVE_CLIENT_SECRET = 'Ve9Hcte_rFey9DNvHM5qD8ypSJCxMjCiBNCuCSDkinHvStS7SHnVmycV9GjaSOjT';
 
 let authData = {
   client_id: NON_INTERACTIVE_CLIENT_ID,
@@ -31,3 +31,61 @@ function getAccessToken(req, res, next){
       }
     })
 }
+app.get('/', function(req, res){
+  res.render('index');
+})
+
+app.get('/movies', getAccessToken, function(req, res){
+  request
+    .get('http://localhost:8080/movies')
+    .set('Authorization', 'Bearer ' + req.access_token)
+    .end(function(err, data) {
+      if(data.status == 403){
+        res.send(403, '403 Forbidden');
+      } else {
+        var movies = data.body;
+        res.render('movies', { movies: movies} );
+      }
+    })
+})
+
+app.get('/authors', getAccessToken, function(req, res){
+  request
+    .get('http://localhost:8080/reviewers')
+    .set('Authorization', 'Bearer ' + req.access_token)
+    .end(function(err, data) {
+      if(data.status == 403){
+        res.send(403, '403 Forbidden');
+      } else {
+        var authors = data.body;
+        res.render('authors', {authors : authors});
+      }
+    })
+})
+
+app.get('/authors', getAccessToken, function(req, res){
+  request
+    .get('http://localhost:8080/reviewers')
+    .set('Authorization', 'Bearer ' + req.access_token)
+    .end(function(err, data) {
+      if(data.status == 403){
+        res.send(403, '403 Forbidden');
+      } else {
+        var authors = data.body;
+        res.render('authors', {authors : authors});
+      }
+    })
+})
+
+app.get('/pending', getAccessToken, function(req, res){
+  request
+    .get('http://localhost:8080/pending')
+    .set('Authorization', 'Bearer ' + req.access_token)
+    .end(function(err, data) {
+      if(data.status == 403){
+        res.send(403, '403 Forbidden');
+      }
+    })
+})
+
+app.listen(3000);
